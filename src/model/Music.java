4269e485147ef3686bac45df91e13f9ac877cef6
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-13 10:34:33 -0300

    Add Music class extending Media with constructor

---
+++ src/model/Music.java
@@ -0,0 +1,8 @@
+package model;
+
+
+public class Music extends Media {
+    public Music(String title, String creator, int duration, Genre genre) {
+        super(title, creator, duration, genre);
+    }
+}
