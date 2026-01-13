b6ce69bb33b3dd227df020b432d0d5822a0c7f8d
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-13 10:36:10 -0300

    Add Podcast class extending Media

--- src/model/Music.java
+++ src/model/Music.java
@@ -1,6 +1,5 @@
 package model;
 
-
 public class Music extends Media {
     public Music(String title, String creator, int duration, Genre genre) {
         super(title, creator, duration, genre);
---
+++ src/model/Podcast.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Podcast extends Media {
+    public Podcast(String title, String creator, int duration, Genre genre) {
+        super(title, creator, duration, genre);
+    }
+}
